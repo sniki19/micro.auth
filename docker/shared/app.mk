7ad47fc84ae33b718a979d70b411b4fb5c88ce4e
sniki19 <artem.kurilik@gmail.com>
2025-08-28 10:56:35 +0300

    UPdate docker build

--- docker/shared/app.mk
+++ docker/shared/app.mk
@@ -1,17 +1,81 @@
-APP_NAME := app_ninja_minotaur
+include .env
+
+
+.PHONY: app.help app.volume.ls app.volume.create app.volume.remove app.up app.down app.start app.stop app.logs app.ps
+
+app.help:
+	@echo "Available commands:"
+	@echo "  app.help          - Show help"
+	@echo "  app.volume.ls     - List volumes"
+	@echo "  app.volume.create - Create volume"
+	@echo "  app.volume.remove - Remove volume"
+	@echo "  app.up            - Start database container"
+	@echo "  app.down          - Stop and remove container"
+	@echo "  app.start         - Start stopped container"
+	@echo "  app.stop          - Stop container"
+	@echo "  app.logs          - View container logs"
+	@echo "  app.ps            - Container status"
+
+
+app.volume.ls:
+	@echo "List of volumes:"
+	docker volume ls | grep -E "(${MICROSERVICE_LOGS_VOLUME_NAME}|NAME)"
+
+app.volume.create:
+	@if ! docker volume inspect ${MICROSERVICE_LOGS_VOLUME_NAME} >/dev/null 2>&1; then \
+		echo "Creating volume ${MICROSERVICE_LOGS_VOLUME_NAME}..."; \
+		docker volume create ${MICROSERVICE_LOGS_VOLUME_NAME}; \
+		echo "Volume ${MICROSERVICE_LOGS_VOLUME_NAME} Created"; \
+	else \
+		echo "Volume ${MICROSERVICE_LOGS_VOLUME_NAME} already exists. Doing nothing"; \
+	fi
+	make app.volume.ls
+
+app.volume.remove:
+	@if docker volume inspect ${MICROSERVICE_LOGS_VOLUME_NAME} >/dev/null 2>&1; then \
+		echo "Removing volume ${MICROSERVICE_LOGS_VOLUME_NAME}..."; \
+		docker volume rm ${MICROSERVICE_LOGS_VOLUME_NAME}; \
+		echo "Volume ${MICROSERVICE_LOGS_VOLUME_NAME} Removed"; \
+	else \
+		echo "Volume ${MICROSERVICE_LOGS_VOLUME_NAME} does not exist. Nothing to remove"; \
+	fi
+	make app.volume.ls
 
 app.build:
-	docker compose build $(APP_NAME)
+	docker compose build microservice
 	docker image ls
+
 app.remove:
 	docker compose down --rmi local
 	docker image ls
 
 app.up:
-	docker compose up -d $(APP_NAME) --build
+	make app.volume.create
+	@echo "Starting app container..."
+	docker compose up -d microservice --build
+	@echo "Container started!"
+	@echo "Access: http://${API_HOST}:${API_PORT}"
+
 app.down:
-	docker compose down $(APP_NAME)
+	@echo "Stopping and removing container..."
+	docker compose down microservice
+	@echo "Container stopped and removed!"
+	make app.volume.remove
+
 app.start:
-	docker compose start $(APP_NAME)
+	@echo "Starting container..."
+	docker compose start microservice
+	@echo "Container started!"
+
 app.stop:
-	docker compose stop $(APP_NAME)
+	@echo "Stopping container..."
+	docker compose stop microservice
+	@echo "Container stopped!"
+
+app.logs:
+	@echo "Viewing container logs..."
+	docker compose logs -f microservice
+
+app.ps:
+	@echo "Container status:"
+	docker compose ps microservice
--- docker/shared/database.mk
+++ docker/shared/database.mk
@@ -1,30 +1,73 @@
-VOLUME_NAME := runesteel_overlord_si1
-DATABASE_NAME := db_runesteel_overlord
+include .env
 
-vol.create:
-	@if ! docker volume inspect $(VOLUME_NAME) >/dev/null 2>&1; then \
-		docker volume create $(VOLUME_NAME); \
-		echo "Volume $(VOLUME_NAME) Created"; \
+
+.PHONY: db.help db.volume.ls db.volume.create db.volume.remove db.up db.down db.start db.stop db.logs db.ps
+
+db.help:
+	@echo "Available commands:"
+	@echo "  db.help          - Show help"
+	@echo "  db.volume.ls     - List volumes"
+	@echo "  db.volume.create - Create volume"
+	@echo "  db.volume.remove - Remove volume"
+	@echo "  db.up            - Start database container"
+	@echo "  db.down          - Stop and remove container"
+	@echo "  db.start         - Start stopped container"
+	@echo "  db.stop          - Stop container"
+	@echo "  db.logs          - View container logs"
+	@echo "  db.ps            - Container status"
+
+
+db.volume.ls:
+	@echo "List of volumes:"
+	docker volume ls | grep -E "(${DATABASE_VOLUME_NAME}|NAME)"
+
+db.volume.create:
+	@if ! docker volume inspect ${DATABASE_VOLUME_NAME} >/dev/null 2>&1; then \
+		echo "Creating volume ${DATABASE_VOLUME_NAME}..."; \
+		docker volume create ${DATABASE_VOLUME_NAME}; \
+		echo "Volume ${DATABASE_VOLUME_NAME} Created"; \
 	else \
-		echo "Volume $(VOLUME_NAME) already exists. Doing nothing"; \
+		echo "Volume ${DATABASE_VOLUME_NAME} already exists. Doing nothing"; \
 	fi
-	docker volume ls
-vol.remove:
-	@if docker volume inspect $(VOLUME_NAME) >/dev/null 2>&1; then \
-		docker volume rm $(VOLUME_NAME); \
-		echo "Volume $(VOLUME_NAME) Removed"; \
+	make db.volume.ls
+
+db.volume.remove:
+	@if docker volume inspect ${DATABASE_VOLUME_NAME} >/dev/null 2>&1; then \
+		echo "Removing volume ${DATABASE_VOLUME_NAME}..."; \
+		docker volume rm ${DATABASE_VOLUME_NAME}; \
+		echo "Volume ${DATABASE_VOLUME_NAME} Removed"; \
 	else \
-		echo "Volume $(VOLUME_NAME) does not exist. Nothing to remove"; \
+		echo "Volume ${DATABASE_VOLUME_NAME} does not exist. Nothing to remove"; \
 	fi
-	docker volume ls
+	make db.volume.ls
 
 db.up:
-	make vol.create
-	docker compose up -d $(DATABASE_NAME)
+	make db.volume.create
+	@echo "Starting database container..."
+	docker compose up -d database
+	@echo "Container started!"
+	@echo "Access: http://${DATABASE_HOST}:${DATABASE_PORT}"
+
 db.down:
-	docker compose down $(DATABASE_NAME)
-	make vol.remove
+	@echo "Stopping and removing container..."
+	docker compose down database
+	@echo "Container stopped and removed!"
+	make db.volume.remove
+
 db.start:
-	docker compose start $(DATABASE_NAME)
+	@echo "Starting container..."
+	docker compose start database
+	@echo "Container started!"
+
 db.stop:
-	docker compose stop $(DATABASE_NAME)
+	@echo "Stopping container..."
+	docker compose stop database
+	@echo "Container stopped!"
+
+db.logs:
+	@echo "Viewing container logs..."
+	docker compose logs -f database
+
+db.ps:
+	@echo "Container status:"
+	docker compose ps database
